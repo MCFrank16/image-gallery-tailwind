1352c27882010ea50ea86e940f3fee63cd1fab4e
MCFrank16 <mecfrank16@gmail.com>
2020-05-03 21:15:41 +0200

    coding the tutorial tailwind

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import ImageCard from './components/imageCard';
 
 function App() {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState('');
+
+  useEffect(() => {
+    fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_API}
+    &q=${term}&image_type=photo&pretty=true`)
+    .then(res => res.json())
+    .then(data => {
+      setImages(data.hits);
+      setIsLoading(false);
+    })
+    .catch(err => console.log(err));
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Hello world.</p>
-      </header>
+    <div className="container mx-auto">
+     <div className="grid grid-cols-3 gap-4">
+       { isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1> :
+         images.map(image => (
+           <ImageCard key={image.id} image={image} />
+         ))
+       }
+     </div>
     </div>
+    
   );
 }
 
